8f56e0be73c509e884eb9be129bda1a9a7311034
Trần Phạm Thanh Phong <127082840+Yuta3108@users.noreply.github.com>
2025-04-09 01:04:39 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ export default {
     },
     extend: {
       backgroundImage: {
-        'bg-test':"url('public/img/bgtest.jpg')"
+        'bg-test':"url('./img/bgtest.jpg')"
       },
     },
     screens: {
