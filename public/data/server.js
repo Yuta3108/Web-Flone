fe6a5c1137bda40596dcde92dcb8a310705ab3b7
Yuta3108 <toyotran0@gmail.com>
2025-04-09 01:34:07 +0700

    check endpoint api

--- public/data/server.js
+++ public/data/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mysql from "mysql2/promise";
-
+import expressListEndpoints from 'express-list-endpoints';
 
 const app = express();
 app.use(cors());
@@ -133,3 +133,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(` Server chạy tại http://localhost:${PORT}`);
 });
+
+console.log(expressListEndpoints(app));
